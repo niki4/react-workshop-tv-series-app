5726c131ef157b97907c85b603d8f74c18bbc775
Ivan Nikiforov <nikiforr@gmail.com>
2019-03-20 17:56:55 +0300

    Initial commit

---
+++ src/components/app/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Main from '../main';
+import './App.css';
+import 'whatwg-fetch';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">TV Series List</h1>
+          <Main />
+        </header>    
+      </div>
+    );
+  }
+}
+
+export default App;
